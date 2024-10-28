d41e4c6e76a94d472e31569e7d948bf10ef5abf9
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-10-28 08:26:52 -0500

    Base de datos

---
+++ OlallaJessica-ExamenP1/Migrations/20241028131519_primera-migracion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OlallaJessica_ExamenP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class primeramigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Celular",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Modelo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Anio = table.Column<int>(type: "int", nullable: false),
+                    Precio = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Celular", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Olalla",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Estatura = table.Column<float>(type: "real", nullable: false),
+                    Mascota = table.Column<bool>(type: "bit", nullable: false),
+                    Nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CelularId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Olalla", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Olalla_Celular_CelularId",
+                        column: x => x.CelularId,
+                        principalTable: "Celular",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Olalla_CelularId",
+                table: "Olalla",
+                column: "CelularId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Olalla");
+
+            migrationBuilder.DropTable(
+                name: "Celular");
+        }
+    }
+}
