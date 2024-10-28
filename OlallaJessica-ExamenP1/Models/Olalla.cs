29052e25465ff2d166de110503fdd617b1e19b5f
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-10-28 07:36:29 -0500

    Variables

---
+++ OlallaJessica-ExamenP1/Models/Olalla.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OlallaJessica_ExamenP1.Models
+{
+    public class Olalla
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [Range(1, 2)]
+        public float Estatura {  get; set; }
+        [Required]
+        public bool Mascota {  get; set; }
+        [Required]
+        public DateTime Nacimiento { get; set; }
+
+    }
+}
